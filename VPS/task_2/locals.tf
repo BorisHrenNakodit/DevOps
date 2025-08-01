e69648c2001d0951f24bd2f442146442ff57791e
Ivan Schucin <ivanschucin@yandex.ru>
2025-08-01 11:20:32 +0300

    VPC_correction

--- VPS/task_2/locals.tf
+++
@@ -1,6 +0,0 @@
-  locals {
-    ssh_connect   ="${var.ssh_all.ssh_vm.ssh_user}:${file(var.ssh_all.ssh_vm.ssh_path)}"
-    nat_ssh_path  ="${file(var.ssh_all.ssh_vm.ssh_path)}"
-    nat_ssh_user  ="${var.ssh_all.ssh_vm.ssh_user}"
-    sub_public    ="${yandex_vpc_subnet.test_vps_subnet.id}"
-  }
